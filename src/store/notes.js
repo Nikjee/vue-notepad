8640efab7224bf9d91a630b6c87f15a24e26c31d
Nikjee <boss.bold@mail.ru>
2022-05-31 20:40:07 +0300

    persistent storage and actions

--- src/store/notes.js
+++ src/store/notes.js
@@ -1,9 +1,10 @@
 import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
 
 export const useNotesStore = defineStore('notes', {
   state: () => {
     return {
-      notes: [
+      notes: useStorage('notes', [
         {
           id: 1,
           title: 'One',
@@ -18,10 +19,22 @@ export const useNotesStore = defineStore('notes', {
           x: 10,
           y: 10,
         },
-      ],
+      ]),
     }
   },
   actions: {
-    addNote: (id, title, text) => {},
+    addNote() {
+      let note = {
+        id: this.notes.length + 1,
+        title: 'Your title',
+        text: 'Your text',
+        x: 300,
+        y: 300,
+      }
+      this.notes.push(note)
+    },
+    removeNote(id) {
+      this.notes.splice(id, 1)
+    },
   },
 })
